d3d467cc97cf9a8a730855f709d74cb12b3eb162
nguyen_thanh_nam <{namnguyenthanh2k1@gmail.com}>
2022-05-18 09:47:40 +0700

    da them option tree dropdown tai trang hocsinh

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -2,29 +2,30 @@ var app = angular.module('myApp', ['ngRoute', 'angularUtils.directives.dirPagina
 
 app.controller('myController', function ($scope, $location) {
     $scope.ListClass = [
-        { id: 200, name: 'Lop 11', parentID: 11, prefix: '', level: 1 },
-        { id: 199, name: 'Lop 11A', parentID: 11, prefix: '__', level: 2 },
-        { id: 189, name: 'Lop 11B', parentID: 11, prefix: '__', level: 2 },
-        { id: 299, name: 'Lop 12A', parentID: 12, prefix: '__', level: 2 },
-        { id: 289, name: 'Lop 12B', parentID: 12, prefix: '__', level: 2 },
-        { id: 179, name: 'Lop 11C', parentID: 11, prefix: '__', level: 2 },
-        { id: 300, name: 'Lop 12', parentID: 12, prefix: '', level: 1 },
-        { id: 288, name: 'Lop 12B', parentID: 12, prefix: '__', level: 2 },
-        { id: 100, name: 'Lop 10', parentID: 10, prefix: '', level: 1 },
-        { id: 189, name: 'Lop 11B1', parentID: 11, prefix: '____', level: 2 },
-        { id: 189, name: 'Lop 11B2', parentID: 11, prefix: '____', level: 2 },
-        { id: 189, name: 'Lop 10B2', parentID: 10, prefix: '____', level: 2 },
-        { id: 189, name: 'Lop 10B1', parentID: 10, prefix: '____', level: 2 },
+        { name: 'Lop', parentID: 0, prefix: '' },
+        { name: 'Lop 11', parentID: 11, prefix: '' },
+        { name: 'Lop 11A', parentID: 11, prefix: '__' },
+        { name: 'Lop 11B', parentID: 11, prefix: '__' },
+        { name: 'Lop 12A1', parentID: 12, prefix: '____' },
+        { name: 'Lop 12A2', parentID: 12, prefix: '____' },
+        { name: 'Lop 11C', parentID: 11, prefix: '__' },
+        { name: 'Lop 12', parentID: 12, prefix: '' },
+        { name: 'Lop 12B', parentID: 12, prefix: '__' },
+        { name: 'Lop 10', parentID: 10, prefix: '' },
+        { name: 'Lop 11A1', parentID: 11, prefix: '____' },
+        { name: 'Lop 11A2', parentID: 11, prefix: '____' },
+        { name: 'Lop 10A2', parentID: 10, prefix: '____' },
+        { name: 'Lop 10A1', parentID: 10, prefix: '____' }
     ];
 
     $scope.students = [
-        { name: 'Nam', age: new Date(2004, 01, 01), class: 'Lop 10A1' },
-        { name: 'Bede', age: new Date(2006, 01, 01), class: 'Lop 10A2' },
-        { name: 'Hoho', age: new Date(2003, 01, 01), class: 'Lop 10A2' },
-        { name: 'Kaka', age: new Date(2002, 01, 01), class: 'Lop 11A1' },
-        { name: 'Benben', age: new Date(2004, 01, 01), class: 'Lop 11A2' },
-        { name: 'Lunu', age: new Date(2006, 01, 01), class: 'Lop 12A1' },
-        { name: 'Lpaa', age: new Date(2001, 01, 01), class: 'Lop 12A2' }
+        { name: 'Nam', age: new Date(2004, 01, 01), klass: 'Lop 10A1' },
+        { name: 'Bede', age: new Date(2006, 01, 01), klass: 'Lop 10A2' },
+        { name: 'Hoho', age: new Date(2003, 01, 01), klass: 'Lop 10A2' },
+        { name: 'Kaka', age: new Date(2002, 01, 01), klass: 'Lop 11A1' },
+        { name: 'Benben', age: new Date(2004, 01, 01), klass: 'Lop 11A2' },
+        { name: 'Lunu', age: new Date(2006, 01, 01), klass: 'Lop 12A1' },
+        { name: 'Lpaa', age: new Date(2001, 01, 01), klass: 'Lop 12A2' }
     ];
 
     $scope.classes = [
@@ -46,12 +47,12 @@ app.controller('myController', function ($scope, $location) {
     $scope.search = function () {
         var tempName = $scope.inputDataSearch.name;
         var tempAge = $scope.inputDataSearch.age;
-        var tempClass = $scope.inputDataSearch.class;
+        var tempClass = $scope.inputDataSearch.klass;
 
         $scope.students = angular.copy(_.filter($scope.studentDefault,
             function (student) {
                 return ((tempAge === null || tempAge === undefined) || (tempAge !== null && tempAge !== undefined && $scope.calculateAge(student.age) == tempAge)) &&
-                    ((tempClass === null || tempClass === undefined) || (tempClass !== null && tempClass !== undefined && student.class.includes(tempClass))) &&
+                    ((tempClass === null || tempClass === undefined) || (tempClass !== null && tempClass !== undefined && student.klass.includes(tempClass))) &&
                     ((tempName === null || tempName === undefined) || (tempName !== null && tempName !== undefined && student.name.includes(tempName)));
             }));
     };
@@ -60,11 +61,18 @@ app.controller('myController', function ($scope, $location) {
         $location.path('/addStudent');
     }
 
-    $scope.saveAddStudent = function (name, age) {
-        if ((name != null || name != undefined) && (age != null || age != undefined)) {
-            $scope.students.push({ name: name, age: age, class: '' });
-            $scope.studentDefault.push({ name: name, age: age, class: '' });
+    $scope.saveAddStudent = function (name, age, klass) {
+        if (
+        (name != null || name != undefined) && 
+        (age != null || age != undefined) && 
+        (klass != null || klass != undefined)
+        ) {
+            var tempStudent = {name: name, age: $scope.calculateAge(age), klass: klass};
+            $scope.students.push(tempStudent);
+            $scope.studentDefault.push(tempStudent);
+            
             $location.path('/student');
+            
         }
     }
 
@@ -73,46 +81,46 @@ app.controller('myController', function ($scope, $location) {
         $location.path('/editStudent');
     }
 
-    $scope.saveEditStudent = function (name, age) {
-        if ((name != null || name != undefined) && (age != null || age != undefined)) {
+    $scope.saveEditStudent = function (name, age, klass) {
+        if ((name != null || name != undefined) && (age != null || age != undefined) && (klass != null || klass != undefined)) {
             var index = $scope.studentDefault.indexOf(name);
 
             $scope.studentDefault.splice(index, 1);
-            $scope.studentDefault.push({ name: name, age: age, class: '' });
+            $scope.studentDefault.push({ name: name, age: age, class: klass });
 
             $location.path('/student');
         }
     }
 
-    $scope.deleteStudent = function (student) {
-        var index = $scope.students.indexOf(student);
-        $scope.students.splice(index, 1);
+    $scope.deleteStudent = function (studentDefault) {
+        var index = $scope.studentDefault.indexOf(studentDefault);
+        $scope.studentDefault.splice(index, 1);
     }
 
-    $scope.addClass = function () {
-        $location.path('/addClass');
-    }
+    // $scope.addClass = function () {
+    //     $location.path('/addClass');
+    // }
 
-    $scope.saveAddClass = function (name) {
-        if (name != null || name != undefined) {
-            $scope.classes.push({ name: name, belong: '' });
-            $location.path('/class');
-        }
-    }
+    // $scope.saveAddClass = function (name) {
+    //     if (name != null || name != undefined) {
+    //         $scope.classes.push({ name: name, belong: '' });
+    //         $location.path('/class');
+    //     }
+    // }
 
-    $scope.editClass = function (classroom) {
-        $scope.class = classroom;
-        $location.path('/editClass');
-    }
+    // $scope.editClass = function (classroom) {
+    //     $scope.class = classroom;
+    //     $location.path('/editClass');
+    // }
 
-    $scope.saveEditClass = function (name) {
-        if (name != null || name != undefined) {
-            $location.path('/class');
-        }
-    }
+    // $scope.saveEditClass = function (name) {
+    //     if (name != null || name != undefined) {
+    //         $location.path('/class');
+    //     }
+    // }
 
-    $scope.deleteClass = function (classroom) {
-        var index = $scope.classes.indexOf(classroom);
-        $scope.classes.splice(index, 1);
-    }
+    // $scope.deleteClass = function (classroom) {
+    //     var index = $scope.classes.indexOf(classroom);
+    //     $scope.classes.splice(index, 1);
+    // }
 });
